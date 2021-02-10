8391e3248760a2010d94c87c2b8188739721941b
rruffer <Devchine@15>
2021-02-10 17:03:27 -0300

    finalizando configuração das api

--- src/main/java/br/com/rruffer/cadastro/beneficio/resource/CadastroResource.java
+++ src/main/java/br/com/rruffer/cadastro/beneficio/resource/CadastroResource.java
@@ -1,15 +1,18 @@
 package br.com.rruffer.cadastro.beneficio.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rruffer.cadastro.beneficio.dto.ClienteDTO;
+import br.com.rruffer.cadastro.beneficio.entity.Cliente;
 import br.com.rruffer.cadastro.beneficio.services.ClienteService;
 
 @CrossOrigin
@@ -21,19 +24,32 @@ public class CadastroResource {
 	private ClienteService clienteService;
 	
 	
-	@GetMapping
-	public ResponseEntity<String> cadastro() {
-		return ResponseEntity.ok().build();
+	@GetMapping("/{cpf}")
+	public ResponseEntity<ClienteDTO> consultarCliente(@PathVariable("cpf") String cpf) {
+		
+		ClienteDTO clienteDTO = null;
+		try {
+			clienteDTO = clienteService.consultar(cpf);
+			if(clienteDTO == null) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return ResponseEntity.ok(clienteDTO);
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> atualizarUsuario(@RequestBody ClienteDTO clienteDTO) {
+	public ResponseEntity<Void> cadastrarCliente(@RequestBody ClienteDTO clienteDTO) {
 		
 		try {
-			clienteService.salvar(clienteDTO);
+			Cliente cliente = clienteService.salvar(clienteDTO);
+			if(cliente == null) {
+				return ResponseEntity.status(420).build();				
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
-			ResponseEntity.status(440).build();
 		}
 		
 		return ResponseEntity.ok().build();
--- src/main/java/br/com/rruffer/cadastro/beneficio/services/ClienteService.java
+++ src/main/java/br/com/rruffer/cadastro/beneficio/services/ClienteService.java
@@ -24,16 +24,33 @@ public class ClienteService {
 	@Autowired
 	private ModelMapper mapper;
 	
+	public ClienteDTO consultar(String cpf) throws Exception {
+		
+		Cliente cliente = clienteRepository.findByCpf(cpf);
+		if(cliente != null) {
+			ClienteDTO clienteDTO = mapper.map(cliente, ClienteDTO.class);			
+			return clienteDTO;
+		}
+
+		return null;
+		
+	}
+	
 	public Cliente salvar(ClienteDTO clienteDTO) throws Exception {
 		
-		Cliente cliente = mapper.map(clienteDTO, Cliente.class);
-		cliente.setId(sequenceGeneratorService.generateSequence(Cliente.SEQUENCE_NAME));
-		clienteRepository.save(cliente);
+		if (clienteRepository.findByCpf(clienteDTO.getCpf()) != null) {
+			return null;
+		} else {
+			Cliente cliente = mapper.map(clienteDTO, Cliente.class);
+			cliente.setId(sequenceGeneratorService.generateSequence(Cliente.SEQUENCE_NAME));
+			clienteRepository.save(cliente);
+			
+			clienteDTO.setId(cliente.getId());
+			clienteProducer.send(clienteDTO);
+			
+			return cliente;
+		}
 		
-		clienteDTO.setId(cliente.getId());
-		clienteProducer.send(clienteDTO);
-		
-		return cliente;
 	}
 	
 	public void salvar(Cliente cliente) throws Exception{
